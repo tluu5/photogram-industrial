e45329f843dd2dbb8ca42ec686e69984a6d877d8
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-10-15 21:14:16 +0000

    adding a default value to user's privacy

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -21,4 +21,5 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: "User", counter_cache: true
   belongs_to :photo
+  validates :body, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_15_194448) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_15_205827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_15_194448) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.citext "username"
-    t.boolean "private"
+    t.boolean "private", default: true
     t.integer "likes_count", default: 0
     t.integer "comments_count", default: 0
     t.datetime "created_at", null: false
