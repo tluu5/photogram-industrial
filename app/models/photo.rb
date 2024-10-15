4f0074da3c7a1597946f8eb5e5b9dfc4fd2e9b4c
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-10-15 21:21:55 +0000

    adding scopes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -26,4 +26,6 @@ class Photo < ApplicationRecord
   has_many :fans, through: :likes
   validates :caption, presence: true
   validates :image, presence: true
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+  scope :by_likes, -> { order(likes_count: :desc) }
 end
