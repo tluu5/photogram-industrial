fd632a0c5a5db51e74de9f2e759a1efc47b95358
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-10-15 19:46:17 +0000

    add photo count to users

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -19,6 +19,6 @@
 #  fk_rails_...  (photo_id => photos.id)
 #
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: "User", counter_cache: true
   belongs_to :photo
 end
